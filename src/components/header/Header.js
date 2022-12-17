c63556dcd4acc3ffea7e44c4d6455645b7526dec
Asaadziad <asaadabuelhija@gmail.com>
2022-12-17 23:15:15 +0200

    Initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="container-fluid header">
+        <h1>CV-Creator V0.1</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
